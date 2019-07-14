be78ecd45c5ad6ee22dcf7d99e25e7360e961bb9
Alberto <alberto.aeraph@gmail.com>
2019-07-14 17:25:35 -0300

    Build

---
+++ dist/modules/equipament/schemas/equipment-repair.schema.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var mongoose_1 = require("mongoose");
+exports.EquipmentRepairSchema = new mongoose_1.Schema({
+    type: String,
+    description: String,
+    createdAt: Date
+});
+//# sourceMappingURL=equipment-repair.schema.js.map

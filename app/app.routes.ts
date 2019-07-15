1f86c6607be59c515c8135e3f5b9cf21e6569194
root <alberto.aeraph@gmail.com>
2019-07-15 04:10:26 +0000

    Add  Equipment Repair Route

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { EquipmentRoutes } from './modules/equipament/routes/equipament.routes'
 import { EquipmentSearchRoutes } from './modules/equipament/routes/equipment-search.routes';
+import { EquipRepairRoutes } from './modules/equipament/routes/equipment-repair.routes';
 let router = Router();
 
 router.use('/equipment', EquipmentRoutes,
-    router.use('/search', EquipmentSearchRoutes)
+    router.use('/search', EquipmentSearchRoutes),
+    router.use('/repair', EquipRepairRoutes)
 );
 
 

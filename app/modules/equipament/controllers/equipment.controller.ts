cf31937d3535cbed7be00b456000b38e8d60e9fd
Alberto <alberto.aeraph@gmail.com>
2019-07-14 22:03:08 -0300

    Create ne CRUD methods for Equipment

--- app/modules/equipament/controllers/equipment.controller.ts
+++ app/modules/equipament/controllers/equipment.controller.ts
@@ -2,6 +2,7 @@ import EquipmentRepository from '../repository/equipment.repository';
 import EquipmentResponse from '../response/equipment.response';
 import { Request, Response } from 'express';
 
+
 class Controller {
     create(req: Request, res: Response) {
         try {
@@ -13,6 +14,58 @@ class Controller {
             console.log('Error on Create Record: ', e);
         }
     }
+
+    findOne(req: Request, res: Response) {
+        try {
+            EquipmentRepository.get(req).subscribe(
+                data => {
+                    res.status(data.status).json(data.equipment);
+                }
+            )
+        } catch (e) {
+            console.log('Error on Find Record: ', e);
+        }
+    }
+
+
+    findAll(req: Request, res: Response) {
+        try {
+            EquipmentRepository.getAll(req,res).subscribe(
+                data => {
+                    res.status(data.status).json(data.equipment);
+                }
+            )
+        } catch (e) {
+            console.log('Error on Find all Record: ', e);
+        }
+    }
+
+
+    update(req: Request, res: Response) {
+        try {
+            EquipmentRepository.update(req).subscribe(
+                data => {
+                    res.status(data.status).json(data.equipment);
+                }
+            )
+        } catch (e) {
+            console.log('Error on Update Record: ', e);
+        }
+    }
+
+    delete(req: Request, res: Response) {
+        try {
+            EquipmentRepository.delete(req).subscribe(
+                data => {
+                    res.status(data.status).json(data.equipment);
+                }
+            )
+        } catch (e) {
+            console.log('Error on Delete Record: ', e);
+        }
+    }
+
+
 }
 
 export default new Controller();
--- app/modules/equipament/repository/equipment.repository.ts
+++ app/modules/equipament/repository/equipment.repository.ts
@@ -10,90 +10,6 @@ import { from, Observable } from 'rxjs';
 class Repository {
     public route: RoutePathService = routerPathService;
 
-    create(req: Request): Observable<any> {
-        try {
-            const Equipament = new EquipmentModel(req.body);
-            return from(
-                Equipament
-                    .save()
-                    .then(equipment => {
-                        return { status: 200, equipment };
-                    })
-                    .catch(error => {
-                        return { status: 500, error };
-                    })
-            );
-        } catch (e) {
-            console.log('Error ao processar a operação: ', e);
-        }
-    }
-
-    update(req): Observable<any> {
-        try {
-            return from(
-                EquipmentModel.findOneAndUpdate(
-                    { _id: req.params.id },
-                    { $set: req.body },
-                    { new: true }
-                )
-                    .then(equipment => {
-                        return { status: 200, equipment };
-                    })
-                    .catch(error => {
-                        return { status: 500, error };
-                    })
-            );
-        } catch (e) {
-            console.log('Error:', e);
-        }
-    }
-    //     /**
-    //      * Update equipment Address
-    //      * @param req 
-    //      */
-    //     updateAddress(req): Observable<any> {
-    //         try {
-    //             return from(
-    //                 Company.findOneAndUpdate(
-    //                     { _id: req.params.companyId, 'address._id': req.params.addressId },
-    //                     { $set: req.body },
-    //                     { new: true }
-    //                 )
-    //                     .then(company => {
-    //                         return { status: 200, company };
-    //                     })
-    //                     .catch(error => {
-    //                         return { status: 500, error };
-    //                     })
-    //             );
-    //         } catch (e) {
-    //             console.log('Error:', e);
-    //         }
-    //     }
-    //     /**
-    //      * Update company Phones
-    //      * @param req 
-    //      */
-    //     updatePhones(req): Observable<any> {
-    //         try {
-    //             return from(
-    //                 Company.findOneAndUpdate(
-    //                     { _id: req.params.companyId, 'phones._id': req.params.phonesId },
-    //                     { $set: req.body },
-    //                     { new: true }
-    //                 )
-    //                     .then(company => {
-    //                         return { status: 200, company };
-    //                     })
-    //                     .catch(error => {
-    //                         return { status: 500, error };
-    //                     })
-    //             );
-    //         } catch (e) {
-    //             console.log('Error:', e);
-    //         }
-    //     }
-
     get(req: Request): Observable<any> {
         try {
             const id: any = req.params.id;
@@ -112,22 +28,109 @@ class Repository {
         }
     }
 
-    async getAll(req: Request, res: Response) {
-        EquipmentModel.find({}, (err, equipment) => {
-            if (err) {
-                return res.status(404).json({
-                    message: 'Não foi possível efetuar a operação',
-                    status: 404
-                });
-            }
-            return res.status(200).json({
-                message: 'Operação realizada com sucesso!!!',
-                status: 200,
-                data: [equipment],
-                url: this.route.getRoute(req)
-            });
-        });
+    getAll(req: Request, res: Response): Observable<any> {
+        return from(
+            EquipmentModel.find().then(
+
+                equipment => {
+
+                    return res.status(200).json({
+                        message: 'Operação realizada com sucesso!!!',
+                        status: 200,
+                        data: [equipment],
+                        url: this.route.getRoute(req)
+                    });
+                }
+            ).catch(err => {
+                if (err) {
+                    return res.status(404).json({
+                        message: 'Não foi possível efetuar a operação',
+                        status: 404
+                    });
+                }
+            })
+        );
+
     }
+
+    create(req: Request): Observable<any> {
+        try {
+            const tags = req.body.title.split(' ');
+            if (tags != null) {
+                req.body.tags = [...tags];
+            }
+            const Equipament = new EquipmentModel(req.body);
+            return from(
+                Equipament
+                    .save()
+                    .then(equipment => {
+                        return { status: 200, equipment };
+                    })
+                    .catch(error => {
+                        return { status: 500, error };
+                    })
+            );
+        } catch (e) {
+            console.log('Error ao processar a operação: ', e);
+        }
+    }
+
+    update(req: Request): Observable<any> {
+        try {
+            return from(
+                EquipmentModel.findByIdAndUpdate(req.params.id,
+                    { $set: req.body },
+                    { new: true }
+                )
+                    .then(equipment => {
+                        return { status: 200, equipment };
+                    })
+                    .catch(error => {
+                        return { status: 500, error };
+                    })
+            );
+        } catch (e) {
+            console.log('Error:', e);
+        }
+    }
+
+    delete(req: Request): Observable<any> {
+        try {
+            return from(
+                EquipmentModel.findByIdAndDelete(req.params.id)
+                    .then(equipment => {
+                        return { status: 200, equipment };
+                    })
+                    .catch(error => {
+                        return { status: 500, error };
+                    })
+            );
+        } catch (e) {
+            console.log('Error:', e);
+        }
+    }
+
+    search(req: Request): Observable<any> {
+        try {
+
+            const search: any = req.params.search;
+            const userRegex = new RegExp(search, 'i')
+
+            return from(
+                EquipmentModel.find({ title: userRegex })
+                    .then(equipment => {
+                        return { status: 200, equipment };
+                    })
+                    .catch(error => {
+                        return { status: 500, error };
+                    })
+            );
+        } catch (e) {
+            console.log('Error on get equipment', e);
+        }
+    }
+
+
 
 }
 

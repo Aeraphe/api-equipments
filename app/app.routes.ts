5ac1406b2be09019e447ebfa04678e9fcb054ea0
root <alberto.aeraph@gmail.com>
2019-07-16 01:09:16 +0000

    Create CRUD Routes Repair Type

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -2,11 +2,15 @@ import { Router } from 'express';
 import { EquipmentRoutes } from './modules/equipament/routes/equipament.routes'
 import { EquipmentSearchRoutes } from './modules/equipament/routes/equipment-search.routes';
 import { EquipRepairRoutes } from './modules/equipament/routes/equipment-repair.routes';
+import { EquipRepairTypeRoutes } from './modules/equipament/routes/equipament-repair-type.routes';
+
 let router = Router();
 
 router.use('/equipment', EquipmentRoutes,
     router.use('/search', EquipmentSearchRoutes),
-    router.use('/repair', EquipRepairRoutes)
+    router.use('/repair', EquipRepairRoutes,
+        router.use('/type', EquipRepairTypeRoutes)
+    )
 );
 
 
